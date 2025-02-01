00232eedba9ee55f09635854e6dc21f0abc8867b
Swetychekkk <stepankursk1@gmail.com>
2025-02-01 16:48:17 +0300

    Reset password added

---
+++ app/src/main/java/com/example/pathfinder/ResetLinkActivity.java
@@ -0,0 +1,58 @@
+package com.example.pathfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pathfinder.databinding.ActivityRegisterBinding;
+import com.example.pathfinder.databinding.ActivityResetLinkBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetLinkActivity extends AppCompatActivity {
+
+    private ActivityResetLinkBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reset_link);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        binding = ActivityResetLinkBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.sendLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.emailResetEt.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Some of important filds cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(binding.emailResetEt.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Check your E-mail", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(ResetLinkActivity.this, LoginActivity.class));
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
